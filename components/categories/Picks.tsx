68ee8a63b3254751947cda86cf15de620b70a0a0
Ahmad Nasser <ahmad_nasser04@outlook.com>
2023-07-04 22:25:23 +0300

    a

--- components/categories/Picks.tsx
+++ components/categories/Picks.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PickCard from "./PickCard";
 import { getCategories } from "@/actions/getCategories";
+import NoResults from "../NoResults";
 
 export default async function Picks() {
   const categories = await getCategories();
@@ -8,6 +9,7 @@ export default async function Picks() {
   return (
     <div className="container space-y-5">
       <h1 className="text-3xl font-bold text-primary">Our Categories</h1>
+      {categories.length === 0 && <NoResults />}
       <div className="grid grid-cols-2 gap-5 lg:grid-cols-3 xl:grid-cols-4">
         {categories.map((pick) => (
           <PickCard key={pick.id} pick={pick} />
